eace8011e90490cee9f8fbb27d3f603f0386a9af
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 02:01:07 -0300

    subida al repo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Hacer un programa para ingresar un número y luego se emita por pantalla un
+         //cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+            int n1;
+           
+            Console.WriteLine("Ingrese un numero");
+            n1 = int.Parse(Console.ReadLine());
+            if(n1>10){
+                    Console.WriteLine("Es mayor a 10");
+                }else{
+                    Console.WriteLine("No es mayor a 10");
+            }
+        }
+    }
+}
